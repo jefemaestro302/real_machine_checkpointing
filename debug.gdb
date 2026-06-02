931eaa3027c082c4bc195227c337b2168784bc25
Daniel Escribano Romero <danielescriromero@gmail.com>
2026-06-02 09:44:04 +0200

    chore: clean up test and junk files

--- debug.gdb
+++
@@ -1,5 +0,0 @@
-run
-bt
-info registers
-x/10i $pc
-x/10g $rsp
--- test.py
+++
@@ -1,6 +0,0 @@
-import struct
-with open("Tailbench/tailbench/silo/tailbench_dump.ckpt", "rb") as f:
-    f.seek(0xb4fff30 + 0x7fedb8)
-    print("Return address at 0x7fedb8 offset: " + hex(struct.unpack("<Q", f.read(8))[0]))
-    f.seek(0xb4fff30 + 0x7fdd90)
-    print("0x7ffff07fdd90: " + hex(struct.unpack("<Q", f.read(8))[0]))
